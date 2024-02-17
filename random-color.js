73006b17ff1997d0eeccadcf31590050f96ecc2e
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-02-17 10:46:27 -0600

    Create random-color.js

---
+++ random-color.js
@@ -0,0 +1,11 @@
+var colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"];
+var randomIndex = Math.floor(Math.random() * 12);
+var pickedColor = [`var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
+
+var elements = document.body.querySelectorAll("*");
+for (var k = 0; k < elements.length; k++) {
+  elements[k].style.backgroundColor = pickedColor[1];
+}
+
+document.body.style.backgroundColor = pickedColor[0];
+document.documentElement.backgroundColor = pickedColor[0];
