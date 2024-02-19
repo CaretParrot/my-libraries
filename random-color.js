c49c85ace2a1baec25441637c511a74d9ac15467
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-02-18 21:06:03 -0600

    Refactored code for gpl.js and random-color.js

--- random-color.js
+++ random-color.js
@@ -1,6 +1,6 @@
 var colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"];
 var randomIndex = Math.floor(Math.random() * 12);
-var pickedColor = [`var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
+var pickedColor = [`var(--${colors[randomIndex]}-50)`, `var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
 
 var elements = document.body.querySelectorAll("*");
 for (var k = 0; k < elements.length; k++) {
