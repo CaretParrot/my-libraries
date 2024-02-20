383819c6181242261fa9f9fba85e0009fa8a02fd
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-02-19 19:08:15 -0600

    Made variables global to fix picked color issues in random-color.js

--- random-color.js
+++ random-color.js
@@ -1,10 +1,13 @@
+const colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"];
+let randomIndex;
+
 let elements = document.body.querySelectorAll("*");
+let pickedColor;
 
 let randomColor = {
     paint: function () {
-        let colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"];
-        let randomIndex = Math.floor(Math.random() * 12);
-        let pickedColor = [`var(--${colors[randomIndex]}-50)`, `var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
+        randomIndex = Math.floor(Math.random() * 12);
+        pickedColor = [`var(--${colors[randomIndex]}-50)`, `var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
 
         for (let k = 0; k < elements.length; k++) {
             elements[k].style.backgroundColor = pickedColor[1];
