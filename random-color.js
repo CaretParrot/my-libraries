d142aaa35e7a3f0d213ed0e08cbfdb6bce79a91a
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-03-06 17:26:17 -0600

    Fixed bug with random-color js object

--- random-color.js
+++ random-color.js
@@ -2,7 +2,7 @@ let randomColor = {
     randomIndex: 0,
     elements: document.body.querySelectorAll("*"),
     pickedColor: {},
-    colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"],
+    colors: ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"],
     
     paint: function (backgroundShade, elementsShade) {
         this.randomIndex = Math.floor(Math.random() * 12);
