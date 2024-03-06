f5ff638645efffa2db3e520c44e63dc8a85ac8ed
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-03-06 13:04:45 -0600

    Updated random-color to be in an object and allow for better color selection

--- random-color.js
+++ random-color.js
@@ -1,19 +1,24 @@
-const colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"];
-let randomIndex;
-
-let elements = document.body.querySelectorAll("*");
-let pickedColor;
-
 let randomColor = {
-    paint: function () {
-        randomIndex = Math.floor(Math.random() * 12);
-        pickedColor = [`var(--${colors[randomIndex]}-50)`, `var(--${colors[randomIndex]}-60)`, `var(--${colors[Math.floor(randomIndex)]}-70)`, `var(--${colors[Math.floor(randomIndex)]}-80)`];
-
-        for (let k = 0; k < elements.length; k++) {
-            elements[k].style.backgroundColor = pickedColor[1];
+    randomIndex: 0,
+    elements: document.body.querySelectorAll("*"),
+    pickedColor: {},
+    colors = ["red", "orange", "yellow", "lime", "green", "seafoam", "teal", "sky", "blue", "purple", "magenta", "pink"],
+    
+    paint: function (backgroundShade, elementsShade) {
+        this.randomIndex = Math.floor(Math.random() * 12);
+        this.pickedColor = {
+            50: `var(--${this.colors[Math.floor(this.randomIndex)]}-50)`,
+            60: `var(--${this.colors[Math.floor(this.randomIndex)]}-60)`,
+            70: `var(--${this.colors[Math.floor(this.randomIndex)]}-70)`,
+            80: `var(--${this.colors[Math.floor(this.randomIndex)]}-80)`,
+            90: `var(--${this.colors[Math.floor(this.randomIndex)]}-90)`,
         }
 
-        document.body.style.backgroundColor = pickedColor[0];
-        document.documentElement.backgroundColor = pickedColor[0];
+        for (let k = 0; k < elements.length; k++) {
+            elements[k].style.backgroundColor = pickedColor.elementsShade;
+        }
+
+        document.body.style.backgroundColor = backgroundShade;
+        document.documentElement.style.backgroundColor = "transparent";
     }
 }
