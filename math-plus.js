f1afe00e76dd022ea7aaa4d2109e20574e44d235
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-04-14 17:22:27 -0500

    Changed structure of math-plus to be within an object

--- math-plus.js
+++ math-plus.js
@@ -1,128 +1,116 @@
-let mathPlusSettings = {
-    rounding: 5
-}
-
-function xroot(value, power) {
-    return Math.pow(value, 1 / power);
-}
-
-function mean(array) {
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-        total += array[i];
-    }
-
-    return total / array.length;
-}
-
-function median(array) {
-    if (array.length % 2 === 0) {
-        return mean([array[array.length / 2 - 1], array[array.length / 2]]);
-    } else {
-        return Math.floor(array[(array.length - 1) / 2]);
-    }
-}
-
-function geoMean(array) {
-    let total = 1;
-    for (let i = 0; i < array.length; i++) {
-        total *= array[i];
-    }
-
-    return Math.pow(total, 1 / array.length);
-}
-
-function isSquare(number) {
-    if (number !== 0 && Math.sqrt(number) % 1 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isCube(number) {
-    if (number !== 0 && Math.cbrt(number) % 1 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function factors(number) {
-    let array = [];
-    for (let i = 1; i < number; i++) {
-        if (number % i === 0) {
-            array.push(i);
+let mathPlus = {
+    settings: {
+        rounding: 5
+    },
+    xroot: function (value, power) {
+        return Math.pow(value, 1 / power);
+    },
+    mean: function (array) {
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            total += array[i];
         }
-    }
 
-    return array;
-}
-
-function convertToFraction(number) {
-    for (let i = 1; i < 1000000; i++) {
-        if ((parseFloat(number) * i) % 1 == 0) {
-            return parseFloat(number) * i + "/" + i;
+        return total / array.length;
+    },
+    median: function (array) {
+        if (array.length % 2 === 0) {
+            return mean([array[array.length / 2 - 1], array[array.length / 2]]);
         } else {
+            return Math.floor(array[(array.length - 1) / 2]);
+        }
+    },
+    geoMean: function (array) {
+        let total = 1;
+        for (let i = 0; i < array.length; i++) {
+            total *= array[i];
+        }
+
+        return Math.pow(total, 1 / array.length);
+    },
+    isSquare: function (number) {
+        if (number !== 0 && Math.sqrt(number) % 1 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    isCube: function (number) {
+        if (number !== 0 && Math.cbrt(number) % 1 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    factors: function (number) {
+        let array = [];
+        for (let i = 1; i < number; i++) {
+            if (number % i === 0) {
+                array.push(i);
+            }
+        }
+
+        return array;
+    },
+    convertToFraction: function (number) {
+        for (let i = 1; i < 1000000; i++) {
+            if ((parseFloat(number) * i) % 1 === 0) {
+                return parseFloat(number) * i + "/" + i;
+            } else {
+                console.warn(
+                    "Fraction is too large to compute. The decimal was returned."
+                );
+                return number;
+            }
+        }
+    },
+    simpRadic: function (radicalSquared) {
+        if (radicalSquared === 1) {
             console.warn(
-                "Fraction is too large to compute. The decimal was returned."
+                "Given radical has only one square factor: 1. The radical is already simplified. The given number was returned."
             );
-            return number;
+            return radicalSquared;
         }
-    }
-}
 
-function simpRadic(radicalSquared) {
-    if (radicalSquared == 1) {
-        console.warn(
-            "Given radical has only one square factor: 1. The radical is already simplified. The given number was returned."
-        );
-        return radicalSquared;
-    }
-
-    if (isSquare(radicalSquared)) {
-        return Math.sqrt(radicalSquared);
-    }
-
-    let squareFactors = [];
-    for (let i = 0; i < factors(radicalSquared).length; i++) {
-        if (isSquare(factors(radicalSquared)[i])) {
-            squareFactors.push(factors(radicalSquared)[i]);
+        if (isSquare(radicalSquared)) {
+            return Math.sqrt(radicalSquared);
         }
-    }
 
-    if (squareFactors.length == 1) {
-        console.warn(
-            "Given radical has only one square factor: 1. The radical is already simplified. The given number was returned."
+        let squareFactors = [];
+        for (let i = 0; i < factors(radicalSquared).length; i++) {
+            if (isSquare(factors(radicalSquared)[i])) {
+                squareFactors.push(factors(radicalSquared)[i]);
+            }
+        }
+
+        if (squareFactors.length === 1) {
+            console.warn(
+                "Given radical has only one square factor: 1. The radical is already simplified. The given number was returned."
+            );
+            return "√" + radicalSquared;
+        }
+
+        let factorToUse = Math.sqrt(Math.max.apply(null, squareFactors));
+        return (
+            factorToUse + "√" + radicalSquared / Math.max.apply(null, squareFactors)
         );
-        return "√" + radicalSquared;
+    },
+    hypotenuse: function (number1, number2) {
+        return Math.sqrt(number1 ** 2 + number2 ** 2);
+    },
+    factorial: function (number) {
+        if (number > 170) {
+            console.warn(
+                'Factorial is too large to compute. Factorial of the given number returned "Infinity".'
+            );
+        }
+        let total = 1;
+        for (let i = 1; i <= number; i++) {
+            total *= i;
+        }
+        return total;
     }
-
-    let factorToUse = Math.sqrt(Math.max.apply(null, squareFactors));
-    return (
-        factorToUse + "√" + radicalSquared / Math.max.apply(null, squareFactors)
-    );
 }
-
-function hypotenuse(number1, number2) {
-    return Math.sqrt(number1 ** 2 + number2 ** 2);
-}
-
-function factorial(number) {
-    if (number > 170) {
-        console.warn(
-            'Factorial is too large to compute. Factorial of the given number returned "Infinity".'
-        );
-    }
-    let total = 1;
-    for (let i = 1; i <= number; i++) {
-        total *= i;
-    }
-    return total;
-}
-
-let e = Math.E;
-let pi = Math.PI;
 
 function sin(x) {
     return Math.sin(x);
