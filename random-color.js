345315e8faa769ba4d4df20a17971c92d27f4c43
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-07-25 09:52:21 -0500

    Removed redundant code

--- random-color.js
+++ random-color.js
@@ -1,19 +1,15 @@
 let randomColor = {
     randomHue: 0,
     elements: document.body.querySelectorAll("*"),
-    pickedElementShade: "",
-    pickedBackgroundShade: "",
 
     paint: function (backgroundShade, elementsShade) {
         this.randomHue = Math.floor(Math.random() * 360);
         this.elements = document.body.querySelectorAll("*");
 
         for (let k = 0; k < this.elements.length; k++) {
-            this.pickedElementShade = `hsl(${this.randomHue}, ${elementsShade}%, ${elementsShade}%)`;
             this.elements[k].style.backgroundColor = this.pickedElementShade;
         }
 
-        this.pickedBackgroundShade = `hsl(${this.randomHue}, ${backgroundShade}%, ${backgroundShade}%)`;
         document.body.style.backgroundColor = this.pickedBackgroundShade;
         document.documentElement.style.backgroundColor = "transparent";
     }
