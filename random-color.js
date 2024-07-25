afb9621dedaec05049df893cbc843296e60a9896
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-07-25 09:50:19 -0500

    Allowed script to save chosen random colors

--- random-color.js
+++ random-color.js
@@ -1,17 +1,20 @@
 let randomColor = {
     randomHue: 0,
     elements: document.body.querySelectorAll("*"),
-    pickedColor: {},
+    pickedElementShade: "",
+    pickedBackgroundShade: "",
 
     paint: function (backgroundShade, elementsShade) {
         this.randomHue = Math.floor(Math.random() * 360);
         this.elements = document.body.querySelectorAll("*");
 
         for (let k = 0; k < this.elements.length; k++) {
-            this.elements[k].style.backgroundColor = `hsl(${this.randomHue}, ${elementsShade}%, ${elementsShade}%)`;
+            this.pickedElementShade = `hsl(${this.randomHue}, ${elementsShade}%, ${elementsShade}%)`;
+            this.elements[k].style.backgroundColor = this.pickedElementShade;
         }
 
-        document.body.style.backgroundColor = `hsl(${this.randomHue}, ${backgroundShade}%, ${backgroundShade}%)`;
+        this.pickedBackgroundShade = `hsl(${this.randomHue}, ${backgroundShade}%, ${backgroundShade}%)`;
+        document.body.style.backgroundColor = this.pickedBackgroundShade;
         document.documentElement.style.backgroundColor = "transparent";
     }
 }
