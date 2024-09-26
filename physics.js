db7e329d07549a0f3b5415aedff944f23f684a51
CaretParrot <wopow29@gmail.com>
2024-09-26 17:45:07 -0500

    Added a physics directory

---
+++ physics.js
@@ -0,0 +1,13 @@
+let physics = {
+    constants: {
+        avogadros: 6.02 * 10 ** 23,
+        universalGas: 8.31,
+        boltzmanns: 1.38 * 10 ** -23,
+        atmosphericPressure: 10 ** 5,
+        coulomb: 9 * 10 ** 9,
+        protonMass: 1.67 * 10 ** -27,
+        neutronMass: 1.67 * 10 ** -27,
+        electronMass: 9.11 * 10 ** -31,
+        elementaryCharge: 1.6 * 10 ** -19
+    }
+}
