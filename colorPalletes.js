8ab4883321bf367eba552e7e6dc2181a2589b1bc
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-10-02 13:07:36 -0500

    Added noPaint class to prevent painting over certain elements

--- colorPalletes.js
+++ colorPalletes.js
@@ -90,7 +90,7 @@ const colorPalletes = {
             textShade1 = 10;
             textShade2 = 20;
         }
-        let allElements = document.querySelectorAll("*");
+        let allElements = document.querySelectorAll(":not(body, script, .noPaint)");
         for (let i = 0; i < allElements.length; i++) {
             allElements[i].style.backgroundColor = colorPalletes[color][10];
             allElements[i].style.color = colorPalletes[color][90];
