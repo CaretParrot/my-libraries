6e5571eb0035decba3a4e97c3d5d85e790e71368
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-10-04 12:49:22 -0500

    Merged color palletes and random color together

--- color-palletes.js
+++ color-palletes.js
@@ -105,3 +105,18 @@ const colorPalletes = {
     }
 }
 
+const randomColor = {
+    randomHue: 0,
+    elements: document.body.querySelectorAll("*"),
+
+    paint: function (backgroundShade, elementsShade) {
+        this.randomHue = Math.floor(Math.random() * 360);
+        this.elements = document.body.querySelectorAll("*");
+
+        for (let k = 0; k < this.elements.length; k++) {
+            this.elements[k].style.backgroundColor = `hsl(${this.randomHue}, ${elementsShade}%, ${elementsShade}%)`;
+        }
+        document.body.style.backgroundColor = `hsl(${this.randomHue}, ${backgroundShade}%, ${backgroundShade}%)`;
+        document.documentElement.style.backgroundColor = "transparent";
+    }
+}
