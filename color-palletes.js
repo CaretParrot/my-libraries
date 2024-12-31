8a085f588ed747f3e854ed498ccab6cf22df79d7
CaretParrot <wopow29@gmail.com>
2024-12-31 15:40:10 -0600

    Added function to save the color after generation

--- color-palletes.js
+++ color-palletes.js
@@ -1,4 +1,5 @@
 const colorPalletes = {
+    savedColor: 0,
     paint: function (color = Math.floor(Math.random() * 360)) {
         document.documentElement.style.setProperty(`--dark-1`, `hsl(${color}, 5%, 10%)`);
         document.documentElement.style.setProperty(`--dark-2`, `hsl(${color}, 10%, 20%)`);
@@ -6,5 +7,6 @@ const colorPalletes = {
         document.documentElement.style.setProperty(`--light-1`, `hsl(${color}, 45%, 90%)`);
         document.documentElement.style.setProperty(`--light-2`, `hsl(${color}, 40%, 80%)`);
         document.documentElement.style.setProperty(`--light-3`, `hsl(${color}, 35%, 70%)`);
+        colorPalletes.savedColor = color;
     }
 }
